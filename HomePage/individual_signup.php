eaf77b58b58a4bb78f5ea1c91f6bfcad36acfc6d
unknown <meghashahri@gmail.com>
2020-11-09 16:00:33 +0530

    individual_signup

--- HomePage/individual_signup.php
+++ HomePage/individual_signup.php
@@ -1,37 +1,45 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Organization SignUp</title>
-        <link rel="stylesheet" href="css/org2.css">
+        <title>Individual's SignUp</title>
+        <link rel="stylesheet" href="css/ind.css">
         <script src="js_files/org2.js"></script>
     </head>
     <body>
         <div class="headings">
         <h1>SiteName</h1>
-        <h2>For Organizations</h2>
-        <p>Find people,connect and help society develop</p>
+        <h2>For Individuals</h2>
+        <p>Find NGO'S,connect and help society develop</p>
     </div>
         <div id="details2-form" class="details2-form">
             <form class="form" method="POST">
                 <div class="form-item">
+                    <div class="personal-details">
+                        <input type="text" id="name" placeholder="First & Last name">
+                        <input type="email" id="email" placeholder="Email">
+                        <input type="password" id="password" placeholder="Password">
+                        <input type="password" id="c_password" placeholder="Confirm Password">
+                        <input type="number" id="age" placeholder="Age">
+                        <input type="tel" id="phone" placeholder="Phone">
+                        <input type="text" id="location" placeholder="Location">
+                    </div>
+
+                </div>
+
+                <div class="form-item">
                     <div class="first-form-item">
-                        <label for="need">Do you need?</label>
+                        <label for="need">Serve as?</label>
                             <select name="need" id="need">
                                 <option value="v">Volunteers</option>
                                 <option value="d">Donors</option>
                                 <option value="vd">Both</option>
                             </select>
                 </div>
-        </div>
-        <div class="form-item">
-            <div class="desc">
-                <label for="desc">Describe your organization:</label>
-                 <textarea rows="4" cols="54" name="textarea_desc" id="textarea_desc" placeholder="Description"></textarea>   
-            </div>         
-        </div>
+             </div>
+       
             <div class="form-item">
                 <div class="second-form-item">
-                <label for="domain">Domain You work on:</label>
+                <label for="domain">Domain of Interest:</label>
                 <select id="domain" multiple onchange='whichDomain(domain)'>
                   
                         <option selected value="e">Education</option>
@@ -39,11 +47,10 @@
                         <option value="c">Cleaning</option>
                         <option value="f">Provide Food</option>
                         <option value="p">For physically disabled</option>
-                        <option value="o">Others</option>
+                        <!-- <option value="o">Others</option> -->
                        
-              
                 </select>
-                <input type="text" id="other_domain" placeholder="Other Domain" style="display:none"></input>
+                <!-- <input type="text" id="other_domain" placeholder="Other Domain" style="display:none"></input> -->
            <p>(Hold down the <b>Ctrl (windows) or Command (Mac)</b> button to select multiple options.)</p> 
         </div>
         </div>
