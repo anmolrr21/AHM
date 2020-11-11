fe633c86084a5df15ef8f8e85d3afbedb7c01d42
Hitesh-Dhameja <65720538+Hitesh-Dhameja@users.noreply.github.com>
2020-11-12 00:14:33 +0530

    Delete script.js

--- js_files/script.js
+++
@@ -1,5 +0,0 @@
-var about = document.getElementById('aboutScroll');
-about.addEventListener('click',()=>{
-    var distance = document.getElementById('about').offsetTop;
-    window.scrollTo(0, distance);
-})
