1b4a380803da6ca182c70a53eb3f4f34470b920c
Hitesh-Dhameja <hitu2085@gmail.com>
2020-11-15 18:48:04 +0530

    New Files

---
+++ homePage.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Home | ConnecTTogether</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    <?php
+        include 'commonNavbar.php';
+    ?>
+
+    <div class="share">
+        <h4><i class="fa fa-pencil-square-o fa-lg" aria-hidden="true"></i> Share an article, photo, video or idea</h4>
+        <ul>
+            <li><i class="fa fa-picture-o" aria-hidden="true" style="color:green"></i><a href="#">Photo</a></li>
+            <li><i class="fa fa-video-camera" aria-hidden="true"></i><a href="#">Video</a></li>
+            <li><i class="fa fa-pencil" aria-hidden="true" style="color:orange"></i><a href="#">Article</a></li>
+            <li><i class="fa fa-file-text" aria-hidden="true" style="color:lightblue"></i><a href="#">Document</a></li>
+
+        </ul>
+    </div>
+</body>
+
+</html>
