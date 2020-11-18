ae5fb10e9b09515533f3695cd650e140a6622c79
unknown <meghashahri@gmail.com>
2020-11-18 23:37:26 +0530

    admin

---
+++ admin.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>ConnecTTogether</title>
+        <link rel="stylesheet" href="css/admin.css">
+        <script src="js_files/admin.js"></script>
+        <!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8=" crossorigin="anonymous"></script>
+        <script src="https://use.fontawesome.com/0cf079388a.js"></script> -->
+    </head>
+    <body>
+        <!----------------------HomePage-------------------------------------------->
+        <section id="header">
+            <ul id="top" class="topnav">
+                <li><img src="images/logo.png" width="90px"></li>
+                <li><a href="index.html">ConnecTTogether</a></li>
+                <li><a class="nav1 active" href="#" >Preview Website</a></li>
+                <li><a class="nav1" href="#about">Log Out</a></li>
+               
+                <div class="menuIcon" onclick="display()">
+                    <div class="bar1"></div>
+                    <div class="bar2"></div>
+                    <div class="bar3"></div>
+                  </div>
+            </ul>
+            <!-- <div id="smallS" class="smallS">
+                <ul id="small" class="small">
+                    <li><a class="nav1 active" href="#" >HOME</a></li>
+                    <li><a class="nav1" href="#about">ABOUT US</a></li>
+                    <li><a class="nav1" href="#features">WHY US?</a></li>
+                    <li><a class="nav1" href="#contact">CONTACT US</a></li>
+                    <li class="right nav1"><a href="#">LOGIN</a></li>
+                </ul>
+            </div> -->
+          
+            
+        </section>
+
+<div class="parts">
+
+        <!------------------left----------------------->
+        <div class="left-part">
+           <ul>
+            <li><a href="#dashboard">DashBoard</a></li>
+            <li><a href="#ad-sect">Approve/Decline Organizations</a><li>
+            <li><a href="#">Delete user</a><li>
+            <li><a href="#">View Complaints</a><li>
+            <li><a href="#">Add Domain</a><li>
+            <li><a href="#">Delete Domain</a><li>
+
+           </ul>
+        </div>
+
+        <div class="right-part">
+            <div class="dashboard" id="dashboard">
+                    <h1>Welcome to admin dashboard</h1>
+                    <div class="row">
+                        <div class="column">
+                            <div class="card">
+                            <h3>Total Users</h3>
+                            <p>150</p>
+                            </div>
+                        </div>
+
+                        <div class="column">
+                            <div class="card">
+                            <h3>Total Organizations</h3>
+                            <p>70</p>    
+                            </div>
+                        </div>
+                        
+                        <div class="column">
+                            <div class="card">
+                            <h3>Total Individuals</h3>
+                            <p>80</p>  
+                        </div>
+                    </div>
+                </div>
+           
+
+
+
+    <!----approve/decline sec----------->
+    <div class="ad-sect" id=ad-sect>
+  
+   
+
+    </div>
+
+    </div>
+    </div>
+    
+ 
+
+        
+        
+        
