6281658d9b894ce628c71cffefbb0a7e7d971913
anmolrr21 <59166802+anmolrr21@users.noreply.github.com>
2020-12-05 04:14:03 +0530

    general

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">  
         <title>ConnecTTogether</title>
         <link rel="stylesheet" href="css/style.css">
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8=" crossorigin="anonymous"></script>
