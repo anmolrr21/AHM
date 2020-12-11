a23d0d19aa6b332fb3f6a80da2cdcfd6c0b17489
Hitesh-Dhameja <hitu2085@gmail.com>
2020-12-11 15:25:45 +0530

    Updates

--- login.php
+++ login.php
@@ -3,6 +3,7 @@
 <head>
         <title>Login</title>
         <link rel="stylesheet" href="css/login.css">
+        <script src="https://use.fontawesome.com/0cf079388a.js"></script>
         
     </head>
 <div class="wrapper fadeInDown">
@@ -20,6 +21,7 @@
     <form class="formm" action="user_login.php" method="POST">
       <input type="email" id="login" class="fadeIn second" name="login1" placeholder="E-mail">
       <input type="password" id="password" class="fadeIn third" name="login2" placeholder="password">
+      <i id="show" class="fa fa-eye" aria-hidden="true"></i>
       <input type="submit" name="login" class="fadeIn fourth" value="Log In">
     </form>
 
@@ -30,4 +32,13 @@
 
   </div>
 </div>
+<script>
+  const togglePassword = document.querySelector('#show');
+  const password = document.querySelector('#password');
+  togglePassword.addEventListener('click', function (e) {
+  const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+  password.setAttribute('type', type);
+  this.classList.toggle('fa-eye-slash');
+});
+</script>
 </html>
